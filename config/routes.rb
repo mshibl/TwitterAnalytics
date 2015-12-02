12f586c2d5f50a485b3e6999db1be14730c6c605
mshibl <mohamed.shibl@hotmail.com>
2015-12-01 20:43:58 -0800

    adds sidekiq web interface

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
 
   resources :users
   get 'users/signup'
+
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
